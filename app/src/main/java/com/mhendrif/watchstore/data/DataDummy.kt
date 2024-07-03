f1a367dd96632f316ad3c1115446e9719056a895
MHendriF <hendrifebriansyah28@gmail.com>
2024-07-03 15:12:58 +0700

    add data dummy

--- app/src/main/java/com/mhendrif/watchstore/data/DataDummy.kt
+++ app/src/main/java/com/mhendrif/watchstore/data/DataDummy.kt
@@ -1,8 +1,37 @@
 package com.mhendrif.watchstore.data
 
+import com.mhendrif.watchstore.R
 import com.mhendrif.watchstore.model.CategoryViewPager
+import com.mhendrif.watchstore.model.Watch
 
 val itemsViewPager = listOf(
     CategoryViewPager(name = "Search Product"),
-    CategoryViewPager(name = "Search Brand"),
+    CategoryViewPager(name = "Brand"),
+)
+
+val itemsWatch = listOf(
+    Watch(
+        name = "Apple Watch",
+        type = "Series 6",
+        price = 3000000,
+        image = R.drawable.watch1
+    ),
+    Watch(
+        name = "Apple Watch",
+        type = "Series 7",
+        price = 3000000,
+        image = R.drawable.watch2
+    ),
+    Watch(
+        name = "Apple Watch",
+        type = "Series 8",
+        price = 3000000,
+        image = R.drawable.watch3
+    ),
+    Watch(
+        name = "Apple Watch",
+        type = "Series 9",
+        price = 3000000,
+        image = R.drawable.watch4
+    )
 )
