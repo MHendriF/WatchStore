6f68b21b79841975d42ff8b51c692020cf3d1f62
MHendriF <hendrifebriansyah28@gmail.com>
2024-07-03 14:21:46 +0700

    add color, data dummy

---
+++ app/src/main/java/com/mhendrif/watchstore/data/DataDummy.kt
@@ -0,0 +1,8 @@
+package com.mhendrif.watchstore.data
+
+import com.mhendrif.watchstore.model.CategoryViewPager
+
+val itemsViewPager = listOf(
+    CategoryViewPager(name = "Search Product"),
+    CategoryViewPager(name = "Search Brand"),
+)
--- app/src/main/java/com/mhendrif/watchstore/ui/theme/Color.kt
+++ app/src/main/java/com/mhendrif/watchstore/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Gray = Color(0xFF9895A6)
+val BluePrimary = Color(0xFF1A73E8)
+val ColorInActivePager = Color(0xFF344054)
